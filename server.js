4542647789f13a4f87e356fdcb5e1d2024d69dbe
Mohamed <alxmohamedemadeldin@gmail.com>
2024-09-04 22:05:37 +0000

    Solved

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-#!/usr/bin/node
+import Express from 'express';
+import router from './routes';
+
+const app = new Express();
+
+app.use(Express.json());
+app.use('/', router);
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
